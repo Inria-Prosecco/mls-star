ced4f46981a0586118bf3a44b85e61af895b1ef5
Jonathan Protzenko <protz@microsoft.com>
2024-01-31 22:33:43 -1000

    WIP restore the JS build

--- js/import.sh
+++ js/import.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+set -e
+set -o pipefail
+set -x
+if [[ $HACL_HOME == "" ]]; then
+  echo "Please define HACL_HOME"
+  exit 1
+fi
+if [[ $HACL_PACKAGES_HOME == "" ]]; then
+  echo "Please define HACL_PACKAGES_HOME"
+  exit 1
+fi
 mkdir -p wasm
 cp $HACL_HOME/dist/wasm/* wasm/
 cp $HACL_PACKAGES_HOME/js/* wasm/
 mkdir -p js
-(cd js && ln -s ../../_build/default/js/MLS_JS.bc.js)
+cd js && cp ../../_build/default/js/MLS_JS.bc.js .
